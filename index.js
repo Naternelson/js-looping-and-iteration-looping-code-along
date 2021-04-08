36bc8111e2432805460ce0541ea020bcc029a581
Nate <7nater.nelson@gmail.com>
2021-04-07 21:31:04 -0600

    commit tree 28020fe43ffe824c22e486d29f299494b856861c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+let writeCards = (names, event) => {
+    let messages = [];
+    for (name of names){
+        messages.push(`Thank you, ${name}, for the wonderful ${event} gift!`)
+    };
+    return messages
+}
+
+let countDown = integer => {
+    while (integer >= 0) {
+        console.log(integer)
+        integer--
+    } 
+}
